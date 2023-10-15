658063d63f43c5cd93f0b3fe0cea6e030a510d69
Anita Pal <anita.pal1687@gmail.com>
2023-10-15 22:02:11 +0800

    commit

---
+++ src/main/java/com/cs/ChklistKioski/ChklistKioskiApplication.java
@@ -0,0 +1,13 @@
+package com.cs.ChklistKioski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChklistKioskiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChklistKioskiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cs/ChklistKioski/ChklistKioskiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs.ChklistKioski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChklistKioskiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
